4cc8873e3c2376193ec5e046b04e5d397d8ddd50
Cong Wu <cw2771@columbia.edu>
2014-04-20 23:26:03 -0400

    AS2_V1
    First commit

---
+++ cachematrix.R
@@ -0,0 +1,37 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## This function 1,set value of the matrix; 2,get value of the matrix
+## 3, set inverse of the matrix 4, get inverse of the matrix
+
+makeCacheMatrix <- function(x = matrix()) {
+  inverse <- NULL
+  set <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(i) inverse <<- i
+  getinverse <- function() inverse
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+
+}
+
+
+##The following function calculates 
+##the inverse of the special matrix created with the above function
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getinverse()
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  data <- x$get()
+  inverse <- solve(data)
+  x$setinverse(inverse)
+  inverse
+}
